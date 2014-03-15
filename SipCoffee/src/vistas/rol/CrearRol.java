78d5565a13f0b8b055ba522a38eb94466fc49d1c
victorm95 <victorm@victorm-G31T-M7>
2014-03-14 23:22:59 -0500

    Se arreglo la clase, pues la version anteriro no servia, no se habia guardado el cambio.

--- SipCoffee/src/vistas/rol/CrearRol.java
+++ SipCoffee/src/vistas/rol/CrearRol.java
@@ -3,18 +3,15 @@ package vistas.rol;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+
+import controladores.RolController;
 
 public class CrearRol extends JPanel {
 
@@ -28,64 +25,33 @@ public class CrearRol extends JPanel {
 	private JButton btnCancelar;
 	private JButton btnEliminar;
 	
-	private JList lista;
+	private JTable roles;
 	
-	private Vector<String> elemtLista;
+	private Vector<String> titulo;
+	private Vector<Vector> data;
+	
+	private RolController controlador;
 	
 	//Constructor
 	public CrearRol(){
+		controlador = new RolController();
 		
-		elemtLista = new Vector<String>();
-		elemtLista.add("Administrador");
-		elemtLista.add("cosechador");
-		elemtLista.add("sembrador");
+		titulo = new Vector<String>();
+		titulo.add("Rol");
+		
+		data = controlador.all();
+		
 		
 		lNombre = new JLabel("Nombre");
-		
 		tNombre = new JTextField(10);
 		
 
-		lista = new JList(elemtLista);
-		
-		
+		roles = new JTable(data,titulo);
 		
 		
 		btnGuardar = new JButton("Guargar");
-		btnGuardar.addActionListener( new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				if(! tNombre.getText().equals("")){
-					elemtLista.add( tNombre.getText() );
-					lista.updateUI();
-					tNombre.setText("");
-				}
-			}
-		});
-		
 		btnCancelar = new JButton("Cancelar");
-		btnCancelar.addActionListener( new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				tNombre.setText("");
-			}
-		});
-		
 		btnEliminar = new JButton("Eliminar");
-		btnEliminar.addActionListener( new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				if(!lista.isSelectionEmpty()){
-					try{
-						elemtLista.remove( lista.getSelectedIndex() ); 
-						lista.updateUI();
-						lista.clearSelection();
-					}catch(Exception ex){
-					}finally{
-						tNombre.setText("");
-					}
-				}
-			}
-		});
 				
 		
 		super.setLayout( new GridBagLayout() );
@@ -97,7 +63,7 @@ public class CrearRol extends JPanel {
 		super.add(btnCancelar , new GridBagConstraints(0,3,1,1,0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(2,10,2,5) ,1,1) );
 		super.add(btnEliminar , new GridBagConstraints(0,4,1,1,0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(2,10,10,2) ,1,1) );
 		
-		super.add(lista , new GridBagConstraints(1,0,2,4,0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(10,10,2,10) ,1,1) );
+		super.add(roles , new GridBagConstraints(1,0,2,4,0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(10,10,2,10) ,1,1) );
 		
 	}
 	
