bb33df97978ed8bb57e41a5d08688a8b24737453
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2014-03-31 21:17:15 -0500

    Se inicia la construccion del Look And Feel(Personalizacion de la interfaz Grafica).

--- SipCoffee/src/vistas/Ventana.java
+++ SipCoffee/src/vistas/Ventana.java
@@ -3,6 +3,8 @@ package vistas;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.synth.SynthLookAndFeel;
 
 public class Ventana extends JFrame {
 	
@@ -14,8 +16,15 @@ public class Ventana extends JFrame {
 		panel =new JPanel();
 		
 		try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ex) {}
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			SynthLookAndFeel look = new SynthLookAndFeel();
+			look.load(Ventana.class.getResourceAsStream("lookAndFeel.xml"),Ventana.class);
+			UIManager.setLookAndFeel(look);
+        } catch (Exception ex) {
+        	try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			}catch (Exception e) {}
+        }
 		
 		super.setContentPane(panel);
 		super.setTitle("SIPCOFFEE");
